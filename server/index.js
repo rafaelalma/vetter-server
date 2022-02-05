3da82cded326b1d68210883f4cf4641c15b797f5
rafaelalma <rafaelalma92@gmail.com>
2022-02-05 11:06:02 +0100

    Implement simple server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('./config/config')
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('<h1>Vetter</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`)
+})
