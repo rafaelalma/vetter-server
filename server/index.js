c8e878481610dd261d3db2c79086e39ad6865412
rafaelalma <rafaelalma92@gmail.com>
2022-02-07 22:20:30 +0100

    Refactor index.js to work with cors and new routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 require('./config/config')
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT
 
@@ -9,16 +10,16 @@ const logger = require('./middleware/logger')
 const { logErrors, clientErrorHandler, errorHandler } = require('./middleware/errors')
 
 const pets = require('./routes/pets')
+const owners = require('./routes/owners')
+const admins = require('./routes/admins')
 
+app.use(cors())
 app.use(express.json())
-
 app.use(logger)
 
-app.get('/', (req, res) => {
-  res.send('<h1>Vetter</h1>')
-})
-
-app.use('/pets', pets)
+app.use('/api/admins', admins)
+app.use('/api/owners', owners)
+app.use('/api/pets', pets)
 
 app.use(logErrors)
 app.use(clientErrorHandler)
