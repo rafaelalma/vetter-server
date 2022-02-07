f4d3923c0c33c11d53c7f885915b1b63f0b04549
rafaelalma <rafaelalma92@gmail.com>
2022-02-07 22:18:50 +0100

    Refactor pets route with some initial data

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -1,8 +1,107 @@
 const express = require('express')
 const router = express.Router()
 
+const pets = [
+  {
+    id: 1,
+    owner_id: 1,
+    name: 'Leo',
+    birth_date: '2015-08-10',
+    species: 'dog',
+    breed: 'pinscher',
+    color: 'black and brown',
+    vaccination_status: 'current',
+    gender: 'male',
+    spayed_or_neutered: 'no'
+  },
+  {
+    id: 2,
+    owner_id: 2,
+    name: 'Alpha',
+    birth_date: '2019-09-10',
+    species: 'dog',
+    breed: 'dobermann',
+    color: 'black and brown',
+    vaccination_status: 'current',
+    gender: 'male',
+    spayed_or_neutered: 'no'
+  },
+  {
+    id: 3,
+    owner_id: 3,
+    name: 'Fify',
+    birth_date: '2020-12-13',
+    species: 'cat',
+    breed: 'unsure',
+    color: 'brown',
+    vaccination_status: 'past due',
+    gender: 'female',
+    spayed_or_neutered: 'spayed'
+  },
+  {
+    id: 4,
+    owner_id: 3,
+    name: 'Fofo',
+    birth_date: '2018-01-02',
+    species: 'cat',
+    breed: 'unsure',
+    color: 'white',
+    vaccination_status: 'current',
+    gender: 'male',
+    spayed_or_neutered: 'neutered'
+  },
+  {
+    owner_id: 5,
+    name: 'Toby',
+    birth_date: '',
+    species: 'dog',
+    breed: 'beagle',
+    color: 'brown and white',
+    vaccination_status: 'past due',
+    gender: 'male',
+    spayed_or_neutered: 'no',
+    id: 5
+  },
+  {
+    owner_id: 5,
+    name: 'Mira',
+    birth_date: '',
+    species: 'cat',
+    breed: '',
+    color: 'brown',
+    vaccination_status: 'current',
+    gender: 'female',
+    spayed_or_neutered: 'no',
+    id: 6
+  },
+  {
+    owner_id: 2,
+    name: 'Beta',
+    birth_date: '',
+    species: 'dog',
+    breed: '',
+    color: '',
+    vaccination_status: 'current',
+    gender: 'male',
+    spayed_or_neutered: 'no',
+    id: 7
+  },
+  {
+    owner_id: 2,
+    name: 'Gamma',
+    birth_date: '',
+    species: 'dog',
+    breed: '',
+    color: '',
+    vaccination_status: 'current',
+    gender: 'male',
+    spayed_or_neutered: 'no',
+    id: 8
+  }
+]
+
 router.get('/', (req, res) => {
-  res.send('<h2>Pets</h2>')
+  res.json(pets)
 })
 
 router.post('/', (req, res) => {
